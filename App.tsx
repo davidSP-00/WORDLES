2f1785b60e77d516f69c7e7d19ffa93c6f762dd7
david <sp.david00@gmail.com>
2022-02-12 22:36:16 -0500

    Text

--- App.tsx
+++ App.tsx
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Dimensions, Platform, SafeAreaView, StyleSheet, Text, StatusBar , View } from 'react-native';
 
 export default function App() {
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+      <View style={styles.wordContainer}>
+      <Text style={styles.inputLetter}>D</Text>
+      <Text style={styles.inputLetter}>A</Text>
+      <Text style={styles.inputLetter}>V</Text>
+      <Text style={styles.inputLetter}>I</Text>
+      <Text style={styles.inputLetter}>D</Text>
+      </View>
+      <View style={styles.wordContainer}>
+      <Text style={styles.inputLetter}>D</Text>
+      <Text style={styles.inputLetter}>A</Text>
+      <Text style={styles.inputLetter}>V</Text>
+      <Text style={styles.inputLetter}>I</Text>
+      <Text style={styles.inputLetter}>D</Text>
+      </View>
+      <View style={styles.wordContainer}>
+      <Text style={styles.inputLetter}>D</Text>
+      <Text style={styles.inputLetter}>A</Text>
+      <Text style={styles.inputLetter}>V</Text>
+      <Text style={styles.inputLetter}>I</Text>
+      <Text style={styles.inputLetter}>D</Text>
+      </View>
+      <View style={styles.wordContainer}>
+      <Text style={styles.inputLetter}>D</Text>
+      <Text style={styles.inputLetter}>A</Text>
+      <Text style={styles.inputLetter}>V</Text>
+      <Text style={styles.inputLetter}>I</Text>
+      <Text style={styles.inputLetter}>D</Text>
+      </View>
+      <View style={styles.wordContainer}>
+      <Text style={styles.inputLetter}>D</Text>
+      <Text style={styles.inputLetter}>A</Text>
+      <Text style={styles.inputLetter}>V</Text>
+      <Text style={styles.inputLetter}>I</Text>
+      <Text style={styles.inputLetter}>D</Text>
+      </View>
+      <View style={styles.wordContainer}>
+      <Text style={styles.inputLetter}>D</Text>
+      <Text style={styles.inputLetter}>A</Text>
+      <Text style={styles.inputLetter}>V</Text>
+      <Text style={styles.inputLetter}>I</Text>
+      <Text style={styles.inputLetter}>D</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#383838',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: 'center',/* 
+    justifyContent: 'center', */
   },
+  wordContainer:{
+    marginVertical: 5,
+    flexDirection: 'row',
+  },
+  inputLetter:{
+
+    backgroundColor: '#FBFBFB',
+    borderWidth:2,
+    borderRadius:5,
+    fontSize:Dimensions.get('window').width/10,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    
+    borderColor:'#B0B0B0',
+    width:Dimensions.get('window').width/6,
+    marginHorizontal:5,
+    height:Dimensions.get('window').width/6
+  }
 });
