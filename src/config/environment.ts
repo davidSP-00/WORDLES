12b1c93487be6d51b812a936f5fb2a8eab8b72d3
David Sullcaray <David Sullcaray@unibanca.com.pe>
2022-12-23 08:19:47 -0500

    conf

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,6 @@
 export const enviroment={
-    /* apiUrl:"http://192.168.18.169:8080", */
-    apiUrl:"http://ionic-prueba-1eba4.uc.r.appspot.com"
+    apiUrl:"http://192.168.18.28:8080",
+    /* apiUrl:"http://ionic-prueba-1eba4.uc.r.appspot.com" */
 
     
 }
